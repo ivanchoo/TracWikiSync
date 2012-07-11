529f226b351efe49983191723e4e04ffd0ab325a
Ivan Choo <hello@ivanchoo.com>
2012-07-11 11:12:42 +0800

    Bumped to v0.3

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="TracWikiSync",
-    version="0.2",
+    version="0.3",
     keywords="trac wiki sync synchronization",
     author="Ivan Choo",
     author_email="hello@ivanchoo.com",
